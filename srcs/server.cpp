e4643b28d228ad908d436e0a7d8d1bf161a989c8
Adem Naouali <anaouali@bess-f2r1s2.clusters.42paris.fr>
2025-03-17 18:10:17 +0100

    lol

--- srcs/server.cpp
+++ srcs/server.cpp
@@ -23,91 +23,108 @@ ServerConfig parseConfig(const std::string &filename){
     return config;
 }
 
+std::string readHtmlFile(const std::string &filename){
+    std::ifstream file(filename);
+    if (!file.is_open()){
+        return "";
+    }
+    std::stringstream buffer;
+    buffer << file.rdbuf();
+    return buffer.str();
+}
+
 int main()
 {
     ServerConfig config = parseConfig("server.conf");
     std::cout << "IP: " << config.ip << ", PORT: " << config.port << std::endl;
+    
     int server_fd = socket(AF_INET, SOCK_STREAM, 0);
-    if (server_fd == -1){
+    if (server_fd == -1) {
         perror("ERROR: Error while creating the server");
         exit(EXIT_FAILURE);
     }
+    
     int opt = 1;
-    if(setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) < 0){
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) < 0) {
         perror("ERROR: Error while activating SO_REUSEADDR");
         close(server_fd);
         exit(EXIT_FAILURE);
     }
+    
     struct sockaddr_in server_address;
     memset(&server_address, 0, sizeof(server_address));
     server_address.sin_family = AF_INET;
     server_address.sin_port = htons(config.port);
-    if (inet_pton(AF_INET, config.ip.c_str(), &server_address.sin_addr) <= 0){
-        perror("ERROR IP CONVERSION");
+    if (inet_pton(AF_INET, config.ip.c_str(), &server_address.sin_addr) <= 0) {
+        perror("ERROR: IP conversion");
         close(server_fd);
         exit(EXIT_FAILURE);
     }
-    if (bind(server_fd, (struct sockaddr *)&server_address, sizeof(server_address)) < 0){
+    
+    if (bind(server_fd, (struct sockaddr *)&server_address, sizeof(server_address)) < 0) {
         perror("ERROR: Error while binding the socket");
         close(server_fd);
         exit(EXIT_FAILURE);
     }
-    if (listen(server_fd, 10) < 0){
-        perror("ERROR: Error while listening");\
+    
+    if (listen(server_fd, 10) < 0) {
+        perror("ERROR: Error while listening");
         close(server_fd);
         exit(EXIT_FAILURE);
     }
-    std::cout << "server launched and listening on " << config.ip 
-                << ":" << config.port << std::endl;
+    
+    std::cout << "Server launched and listening on " << config.ip 
+              << ":" << config.port << std::endl;
+    
     std::vector<pollfd> fds;
     fds.push_back({server_fd, POLLIN, 0});
-    while(true)
-    {
-        int ret = poll(fds.data(), fds.size(), -1);
-        if (ret < 0){
-            perror("ERROR: pol Error");
+    
+    while (true) {
+        int pol_ret = poll(fds.data(), fds.size(), -1);
+        if (pol_ret < 0) {
+            perror("ERROR: poll Error");
             break;
         }
-    }
-    for (size_t i = 0; i <fds.size(); ++i){
-        if (fds[i].revents & POLLIN){
-            if(fds[i].fd == server_fd){
-                sockaddr_in client_addr;
-                socklen_t client_len = sizeof(client_addr);
-                int client_fd = accept(server_fd, (struct sockaddr *)&client_addr, &client_len);
-                if (client_fd >= 0){
-                    std::cout << "New connexion accepted" << std::endl;
-                    fds.push_back({client_fd, POLLIN, 0});
-                }
-            }else{
-                char buffer[1024];
-                int bytes_read = recv(fds[i].fd, buffer, sizeof(buffer) - 1, 0);
-                if (bytes_read > 0){
-                    buffer[bytes_read] = '\0';
-                    std::cout << "Message received: " << buffer << std::endl;
-                    std::string response = "HTTP/1.1 200 OK\r\nContent-Length: 13\r\n\r\nHello, World!";
+        
+        for (size_t i = 0; i < fds.size(); ++i) {
+            if (fds[i].revents & POLLIN) {
+                if (fds[i].fd == server_fd) {
+                    sockaddr_in client_addr;
+                    socklen_t client_len = sizeof(client_addr);
+                    int client_fd = accept(server_fd, (struct sockaddr *)&client_addr, &client_len);
+                    if (client_fd >= 0) {
+                        std::cout << "New connexion accepted" << std::endl;
+                        fds.push_back({client_fd, POLLIN, 0});
+                    } else {
+                        perror("ERROR: accept failed");
+                    }
+                } else {
+                    char buffer[1024];
+                    int bytes_read = recv(fds[i].fd, buffer, sizeof(buffer) - 1, 0);
+                    if (bytes_read > 0) {
+                        buffer[bytes_read] = '\0';
+                        std::cout << "Message received: " << buffer << std::endl;
+                        std::string htmlContent = readHtmlFile("cube.html");
+                        if (htmlContent.empty()){
+                            htmlContent = "<h1>Erreur 404: Page non trouvée</h1>";
+                        }
+                        std::string response = "HTTP/1.1 200 OK\r\n"
+                        "Content-Type: text/html\r\n"
+                        "Content-Length: " + std::to_string(htmlContent.size()) + "\r\n"
+                        "\r\n" +
+                        htmlContent;
                     send(fds[i].fd, response.c_str(), response.size(), 0);
-                }else{
-                    std::cout << "Deconnected client" << std::endl;
-                    close(fds[i].fd);
-                    fds.erase(fds.begin() + i);
-                    --i;
+                    } else {
+                        std::cout << "Disconnected client" << std::endl;
+                        close(fds[i].fd);
+                        fds.erase(fds.begin() + i);
+                        --i;
+                    }
                 }
             }
         }
     }
-
-    // while (true) {
-    //     struct sockaddr_in client_address;
-    //     socklen_t client_addrlen = sizeof(client_address);
-    //     int client_fd = accept(server_fd, (struct sockaddr *)&client_address, &client_addrlen);
-    //     if (client_fd < 0){
-    //         perror("ERROR: connexion probleme");
-    //         continue;
-    //     }
-    //     std::cout << "New connexion accepted" << std::endl;
-    //     //pol
-    //     close(client_fd);
-    // }
+    
     close(server_fd);
+    return 0;
 }
